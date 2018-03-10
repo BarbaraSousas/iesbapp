34a2a9d5baaf6b8cd604c5726f0a73a0bfdf7e22
Barbara <barbarasousaas@gmail.com>
2018-03-09 21:46:26 -0300

    Map commit

--- app/src/main/java/com/example/a1614290070/iesbapp/MainActivity.java
+++ app/src/main/java/com/example/a1614290070/iesbapp/MainActivity.java
@@ -4,12 +4,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng iesbSul = new LatLng(-15.7571194, -47.878842);
+        mMap.addMarker(new MarkerOptions().position(iesbSul).title("IESB sul"));
     }
 }
